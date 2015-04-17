3b84c1edfd9f0bc3528e37b09e4632b48fba34c8
gubatron <gubatron@gmail.com>
2015-04-17 14:09:27 -0400

    frostwire 1.5.2 build 185

--- src/com/frostwire/android/core/Constants.java
+++ src/com/frostwire/android/core/Constants.java
@@ -38,7 +38,7 @@ public final class Constants {
 
     /** should manually match the manifest, here for convenience so we can ask for it from static contexts without
      * needing to pass the Android app context to obtain the PackageManager instance.  */
-    public static final String FROSTWIRE_BUILD = "184";
+    public static final String FROSTWIRE_BUILD = "185";
 
     public static final boolean IS_GOOGLE_PLAY_DISTRIBUTION = false; // true -> FrostWire Basic, false -> FrostWire Plus.
 
